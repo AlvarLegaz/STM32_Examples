dd572c975940a26b6178fec5491e805f8ffd3a7f
Álvar Legaz Aparicio <116595540+AlvarLegaz@users.noreply.github.com>
2025-03-17 11:18:48 +0100

    Add files via upload

---
+++ Controlador_Menu_Servicio_v1/Core/Inc/LCD_i2c.h
@@ -0,0 +1,31 @@
+
+#ifndef LCD_I2C_H_
+#define LCD_I2C_H_
+
+#include "stm32F0xx_hal.h"
+
+#define LCD_CLEAR        0x01
+#define LCD_HOME    0x02
+
+#define LCD_DISPLAY_OFF  0x08
+#define LCD_DISPLAY_ON   0x0C
+#define LCD_CURSOR_ON    0x0E
+#define LCD_CURSOR_BLINK 0x0F
+
+#define LCD_CURSOR_SHIFT_RIGHT  0x14
+#define LCD_CURSOR_SHIFT_LEFT   0x10
+#define LCD_DISPLAY_SHIFT_RIGHT 0x18
+#define LCD_DISPLAY_SHIFT_LEFT  0x1C
+
+void LCD_Init(void);                  // Initialize The LCD For 4-Bit Interface
+void LCD_Clear(void);                 // Clear The LCD Display
+void LCD_SL(void);                    // Shift The Entire Display To The Left
+void LCD_SR(void);                    // Shift The Entire Display To The Right
+void LCD_CMD(char);      // Send Command To LCD
+void LCD_DATA(char);     // Send 4-Bit Data To LCD
+void LCD_Set_Cursor(unsigned char, unsigned char);  // Set Cursor Position
+void LCD_Write_Char(char);        // Write Character To LCD At Current Position
+void LCD_Write_String(char*);     // Write A String To LCD
+
+#endif /* LCD_I2C_H_ */
+
---
+++ Controlador_Menu_Servicio_v1/Core/Src/main.c
@@ -0,0 +1,322 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "LCD_i2c.h"
+
+I2C_HandleTypeDef hi2c1;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+
+#define NO_PULSADO 1
+#define PULSADO 0
+#define BOTON_AZUL 1
+#define BOTON_NEGRO 2
+
+enum estados{Menu_Inicio, Menu_Tem_Hum, Menu_Led, Final_Menu};
+
+uint8_t boton_azul_anterior;
+uint8_t boton_azul_actual;
+uint8_t menu_actual;
+
+uint8_t readButtons(){
+
+	// Codifica los botones que estan pulsados y devuelve el codigo de pulsacion de todos.
+	uint8_t tipo_boton = 0;
+	uint8_t boton_azul_actual = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
+
+	if (boton_azul_anterior == NO_PULSADO && boton_azul_actual == PULSADO){
+		tipo_boton = BOTON_AZUL;
+		boton_azul_anterior = PULSADO;
+	}
+	boton_azul_anterior = boton_azul_actual;
+
+	return tipo_boton;
+}
+
+uint8_t getTemp(){
+	return 31;
+}
+
+uint8_t getHum(){
+	return 50;
+}
+
+void menuInicio(){
+
+	LCD_Clear();
+
+	LCD_Set_Cursor(0, 0);
+	LCD_Write_String("HOLA A TODOS!");
+
+	LCD_Set_Cursor(1, 0);
+	LCD_Write_String("Plsa azul xa sig.");
+
+	while(readButtons() != BOTON_AZUL){
+		;; // Espera pulsacion
+	}
+	menu_actual = (menu_actual + 1) % Final_Menu;
+}
+
+void mostrarTemHum(){
+
+	LCD_Clear();
+
+	LCD_Set_Cursor(0, 0);
+	LCD_Write_String("Temp=");
+	LCD_Set_Cursor(0, 9);
+    LCD_Write_String("Hum= ");
+
+    LCD_Set_Cursor(1, 0);
+    LCD_Write_String("Plsa azul xa sig.");
+
+	while(readButtons() != BOTON_AZUL){
+		;; // Espera pulsacion
+	}
+	menu_actual = (menu_actual + 1) % Final_Menu;
+}
+
+void menuEncendidoLed(){
+
+	LCD_Clear();
+	LCD_Set_Cursor(0, 0);
+	LCD_Write_String("Encendido LED");
+	LCD_Set_Cursor(1, 0);
+	LCD_Write_String("Plsa azul xa sig.");
+
+	while(readButtons() != BOTON_AZUL){
+		;; // hago cosas
+		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+		HAL_Delay(100);
+	}
+
+	menu_actual = (menu_actual + 1) % Final_Menu;
+}
+
+
+int main(void)
+{
+
+  HAL_Init();
+
+  SystemClock_Config();
+
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  LCD_Init();
+
+  boton_azul_anterior = 0;
+  boton_azul_actual = 0;
+  menu_actual = 0;
+
+  while (1)
+  {
+	  switch (menu_actual){
+
+	  	  case Menu_Inicio:
+	  		  menuInicio();
+	  		  break;
+
+	  	  case Menu_Tem_Hum:
+	  		  mostrarTemHum();
+	  		  break;
+
+	  	  case Menu_Led:
+	  		  menuEncendidoLed();
+	  		  break;
+
+	  	  default:
+	  		  menu_actual = 0;
+	  		  break;
+	  };
+
+  }
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x00201D2B;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : USART_TX_Pin USART_RX_Pin */
+  GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF1_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
